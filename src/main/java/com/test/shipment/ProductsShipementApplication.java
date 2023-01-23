45e3469f8b81d922a54dcd6c1043a35611d178c4
andres9906 <stivenaereo@gmail.com>
2023-01-23 17:24:13 -0500

    initial commit

---
+++ src/main/java/com/test/shipment/ProductsShipementApplication.java
@@ -0,0 +1,13 @@
+package com.test.shipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsShipementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsShipementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/shipment/ProductsShipementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.shipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsShipementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
